d33cbf400694c0f37337a2a086e8ae8327727aca
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2024-11-10 21:02:24 +0100

    docker

---
+++ Makefile
@@ -0,0 +1,9 @@
+install:
+	docker compose build
+	docker compose up -d
+	docker compose exec -t php chmod -R 755 /var/www/storage
+	docker compose exec -t php composer install
+	docker compose exec -t php php artisan migrate --seed
+
+shell:
+	docker compose exec -it php sh
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use function Laravel\Prompts\password;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        $user = User::firstOrNew([
+            'email' => "test@test.pl",
+        
+        ]);
+
+        $user->password = Hash::make('test@test.pl');
+        $user->remember_token = Str::random(10);
+        $user->name = fake()->name();
+        $user->email_verified_at = now(); 
+        $user->save();
+    }
+}
