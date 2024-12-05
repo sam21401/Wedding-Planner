e65f633b67ef63e67fc43ad104709ef5eb17c072
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2024-12-05 20:13:31 +0100

    update + small fixes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+Route::middleware(['web'])->group(function () {
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+});
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 require __DIR__.'/auth.php';
+
+
