fdbd22131a0c6990526b8168eda08524662c5c2e
Paweł Żebrowski <pawel.zebrowski@studenci.collegiumwitelona.pl>
2025-01-05 17:59:29 +0100

    Revert "frontend"
    This reverts commit 063f239a23a18e4e194b5fd8be0a0231d55f8c1c.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\HomeController; // Import HomeController
 
-// Obsługa logowania
 Route::middleware(['web'])->group(function () {
     Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
 });
 
-// Strona główna
-Route::get('/', [HomeController::class, 'index']);
-
-// Import dodatkowych tras, jeśli są potrzebne
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 require __DIR__.'/auth.php';
+
+
